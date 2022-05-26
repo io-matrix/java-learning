487da62621d1e72777ee2e3215838853e6fcce6a
Feng Liu <liufenix@foxmail.com>
2022-05-26 15:11:38 +0800

    使用java函数替换传统if-else

---
+++ src/main/java/com/java/java17/function/replaceIfElse/CartPredicates.java
@@ -0,0 +1,10 @@
+package com.java.java17.function.replaceIfElse;
+
+import java.util.function.Predicate;
+
+public class CartPredicates {
+
+    public static final Predicate<Cart> WHEN_CART_VALUE_IS_SIX = cart -> cart.getValue() == 6;
+    public static final Predicate<Cart> WHEN_CART_VALUE_IS_SEVEN = cart -> cart.getValue() == 7;
+
+}
